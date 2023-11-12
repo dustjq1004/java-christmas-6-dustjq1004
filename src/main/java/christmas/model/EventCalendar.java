30152d6098d60758dadbd448b4466dae6df520a3
dustjq1004 <dustjq1005@naver.com>
2023-11-12 18:14:27 +0900

    fix : 디데이 할인 계산과 맞추기 위해 크리스마스 디데이를 가져오는 메서드에 더하기 1하도록 수정

--- src/main/java/christmas/model/EventCalendar.java
+++ src/main/java/christmas/model/EventCalendar.java
@@ -25,7 +25,8 @@ public class EventCalendar {
     }
 
     public int getDDayChristmas(final int day) {
+        final int plus = 1;
         return (int) ChronoUnit.DAYS.between(LocalDate.of(EVENT_YEAR, EVENT_MONTH, day)
-                , LocalDate.of(EVENT_YEAR, EVENT_MONTH, CHRISTMAS_DAY));
+                , LocalDate.of(EVENT_YEAR, EVENT_MONTH, CHRISTMAS_DAY)) + plus;
     }
 }
