87e0ca849ae69f9414efabe736f628175d710fe7
dustjq1004 <dustjq1005@naver.com>
2023-11-12 20:19:33 +0900

    feat : 클라이언트 예약 정보 기능을 가진 클래스 추가

---
+++ src/main/java/christmas/model/Reservation.java
@@ -0,0 +1,46 @@
+package christmas.model;
+
+import java.time.DayOfWeek;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Reservation {
+
+    private final List<Menu> foods;
+    private final Money money;
+    private final int day;
+    private final Map<String, Integer> discountType;
+
+    public Reservation(List<Menu> foods, Money money, int day) {
+        this.foods = foods;
+        this.money = money;
+        this.day = day;
+        discountType = new HashMap<>();
+    }
+
+    public void addDiscountType(String name, int price) {
+        discountType.put(name, price);
+    }
+
+    public boolean isContainDayRange(final int startDay, final int endDay) {
+        return startDay <= day && day <= endDay;
+    }
+
+    public int getChristmasDday() {
+        EventCalendar eventCalendar = new EventCalendar();
+        return eventCalendar.getDDayChristmas(day);
+    }
+
+    public DayOfWeek getDayofWeek() {
+        EventCalendar eventCalendar = new EventCalendar();
+        return eventCalendar.getDayOfWeek(day);
+    }
+
+    public int countFoodType(FoodType foodType) {
+        return (int) foods.stream().filter((food) -> food.getType() == foodType).count();
+    }
+
+}
+
+
