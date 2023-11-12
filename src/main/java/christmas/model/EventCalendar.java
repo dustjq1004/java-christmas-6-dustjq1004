c7c95b370ec6fd4cc6f0f2ed3ff00422f0dd017b
dustjq1004 <dustjq1005@naver.com>
2023-11-12 17:49:32 +0900

    test : 이벤트 날짜 정보를 가져오는 클래스 추가 - 입력된 날짜의 요일을 가져온다. - 입력된 날짜의 별 여부를 가져온다. - 입력된 날짜의 크리스마스 디데이를 가져온다.

---
+++ src/main/java/christmas/model/EventCalendar.java
@@ -0,0 +1,31 @@
+package christmas.model;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class EventCalendar {
+
+    private static final List hasStarDays = List.of(3, 10, 17, 24, 25, 31);
+    private static final int EVENT_YEAR = 12;
+    private static final Month EVENT_MONTH = Month.DECEMBER;
+    private static final int CHRISTMAS_DAY = 25;
+
+    public EventCalendar() {
+    }
+
+    public DayOfWeek getDayOfWeek(final int day) {
+        return LocalDate.of(EVENT_YEAR, EVENT_MONTH, day).getDayOfWeek();
+    }
+
+    public boolean hasStar(final int day) {
+        return hasStarDays.contains(day);
+    }
+
+    public int getDDayChristmas(final int day) {
+        return (int) ChronoUnit.DAYS.between(LocalDate.of(EVENT_YEAR, EVENT_MONTH, day)
+                , LocalDate.of(EVENT_YEAR, EVENT_MONTH, CHRISTMAS_DAY));
+    }
+}
